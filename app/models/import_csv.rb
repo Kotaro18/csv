4de2af1fedbedc9bfc2ed2c0555114b389af1df6
Kotaro <irodori5mile18@gmail.com>
2019-11-13 19:57:29 +0900

    increase list

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -1,12 +1,14 @@
 class ImportCsv < ApplicationRecord
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
-     CSV.foreach(path, headers: true) do |row|
-       User.create(
+    list = []
+    CSV.foreach(path, headers: true) do |row|
+      list << {
          name: row["name"],
          age: row["age"],
          address: row["address"]
-       )
-     end
+      }
+    end
+    User.create(list)
   end
 end
